1188e9535d0b49a71c42b732058229e1cc52904e
P.Ivo <pivolluz@gmail.com>
2024-09-05 14:41:06 -0300

    Iniciando serviço de email

---
+++ service_email/src/main/java/com/empiricus/service_email/ServiceEmailApplication.java
@@ -0,0 +1,13 @@
+package com.empiricus.service_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceEmailApplication.class, args);
+	}
+
+}
